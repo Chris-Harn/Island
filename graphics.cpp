aa403d9af4cf2d74d1cd2d39e436e3606c41d8dd
Christopher Harn <chris.harn@gmail.com>
2013-05-31 19:27:04 -0700

    Fixed memory leak.

--- graphics.cpp
+++ graphics.cpp
@@ -15,7 +15,7 @@ Graphics::Graphics() {
 
 	Running = true;
 	
-	Font = TTF_OpenFont( "arial.ttf", 12 );\
+	Font = TTF_OpenFont( "arial.ttf", 12 );
 	
 	assert( Font != NULL && "arial.ttf failed to load." );
 }
@@ -23,6 +23,7 @@ Graphics::Graphics() {
 Graphics::~Graphics() {
 	printf( "Closing down all systems.\n" );
 	SDL_FreeSurface( Window );
+	SDL_FreeSurface( WorkSurface );
 	TTF_CloseFont( Font );
 
 	TTF_Quit();
@@ -68,8 +69,16 @@ void Graphics::Stage1Generation() {
 	// Generates a 16 by 16 map that will serve as the base for the island
 	// 0 = water, 1 = sand, 2 = grass, 3 = forest, 4 = rock, 5 = mountain
 	Uint8 board[ 16 ][ 16 ];
+	Uint8 board[ 128 ][ 128 ];
+
+
+	for( int x = 0; x < 16; x++ ) {
+		for( int y = 0; y < 16; y++ ) {
+			board[ x ][ y ] = 0;
+		}
+	}
 	
-	// Draw the inital 8 by 8 square in the middle
+	// Draw the inital 8 by 8 square of grass in the middle
 	for( int x = 0; x < 8; x++ ) {
 		for( int y = 0; y < 8; y++ ) {
 			board[ x + 6 ][ y + 6 ] = 2;
